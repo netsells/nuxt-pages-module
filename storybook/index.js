a64f3d643ba60df6919649e35d20e7812c8608e2
Sam Turrell <sam.turrell@netsells.co.uk>
2021-03-31 12:44:06 +0100

    🔧 fixed imports

--- storybook/index.js
+++ storybook/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import '../dist/attr';
-import { WysiwygAttr } from '../dist/components/WysiwygAttr';
+import '../lib/attr';
+import { WysiwygAttr } from '../lib/components/WysiwygAttr';
 
 Vue.component('wysiwyg-attr', WysiwygAttr({}))
